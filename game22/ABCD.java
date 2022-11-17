8864de5a893379fa5dae70688d9a06993ac9473f
tokaihakodatekosen <tokai@hakodate.kosen-ac.jp>
2022-11-17 10:13:11 +0900

    Aがキャラ移動処理を追加

--- game22/ABCD.java
+++ game22/ABCD.java
@@ -15,5 +15,9 @@ public class ABCD extends Actor
     public void act() 
     {
         // Add your action code here.
+        if( Greenfoot.isKeyDown( "up" ) ){
+            setRotation(-90);
+            move(1);
+        }
     }    
 }
